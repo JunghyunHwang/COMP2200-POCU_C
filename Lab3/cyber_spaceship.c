92f43e46b52c6f40d8222344f3f83fe750e7e737
Ja_Hwang <dmagk560@gmail.com>
2022-01-18 22:32:26 +0900

    Fix complie errors

--- Lab3/cyber_spaceship.c
+++ Lab3/cyber_spaceship.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 #include "cyber_spaceship.h"
 
-const char* get_longest_safe_zone_or_null(
-    const char* const cab_start_location, 
-    const size_t cab_length, 
-    const char* const cluster_start_locations[], 
-    const size_t cluster_lengths[], 
-    const size_t cluster_count, 
-    size_t* out_longest_safe_area_length)
+#define CAB_LENGTH (50)
+
+const char* get_longest_safe_zone_or_null(const char* const cab_start_location, const size_t cab_length, const char* const cluster_start_locations[], const size_t cluster_lengths[], const size_t cluster_count, size_t* out_longest_safe_area_length)
 {
-    char multiverse_cab[cab_length] = { 0, };
-    int safe_zone_index[cab_length];
-    int safe_zone_length[cab_length];
+    char multiverse_cab[30] = { 0, };
+    int safe_zone_index[30];
+    int safe_zone_length[30];
     int longest_length;
-    int safe_area_start_index;
+    char* result_address;
     size_t i;
     size_t j;
 
     int universe_distance = cab_start_location - multiverse_cab;
     size_t safe_zone_count = 0;
+    int index = 0;
 
     if (cab_length == 0) {
         return NULL;
@@ -26,11 +23,11 @@ const char* get_longest_safe_zone_or_null(
     
     /* Copy CAB */
     for (i = 0; i < cluster_count; i++) {
-        char* multiverse_cluster_start_location = cluster_start_locations[i] - universe_distance;
+        char* multiverse_cluster_start_location = (char*)cluster_start_locations[i] - universe_distance;
 
         for (j = 0; j < cluster_lengths[i]; j++) {
             char* cluster_address = multiverse_cluster_start_location + j;
-            &temp_address += 1;
+            *cluster_address += 1;
         }
     }
 
@@ -43,28 +40,30 @@ const char* get_longest_safe_zone_or_null(
                 safe_zone_index[j] = i;
             }
 
-            zone_count++;
+            safe_zone_count++;
         } else {
             if (i != 0 && multiverse_cab[i - 1] % 2 == 0) {
-                safe_zone_count[j] = zone_count;
+                safe_zone_length[j] = safe_zone_count;
                 j++;
             }
 
-            zone_count = 0;
+            safe_zone_count = 0;
         }
     }
 
     /* Check longest safe area */
-    longest_length = safe_zone_count[0]
+    longest_length = safe_zone_length[0];
     index = safe_zone_index[0];
 
     for (i = 1; i < j; i++) {
-        if (safe_zone_count[i] >= longest_length) {
+        if (safe_zone_length[i] >= longest_length) {
+            longest_length = safe_zone_length[i];
             index = safe_zone_index[i];
         }
     }
 
-    out_longest_safe_area_length = multiverse_cab + universe_distance + index;
+    *out_longest_safe_area_length = longest_length;
+    result_address = multiverse_cab + universe_distance + index;
 
-    return out_longest_safe_area_length;
+    return result_address;
 }
--- Lab3/main.c
+++ Lab3/main.c
@@ -1,16 +1,12 @@
+#include <stdio.h>
+#include "cyber_spaceship.h"
+
 int main(void)
 {
-    /* 
-    아래의 코드는 다음과 같이 CAB를 설정함. -는 1바이트를 의미
-         0                            29
-    CAB      ------------------------------
-    Cluster1             ---------------
-    Cluster2    -------------------------
-    */
     const char cyber_asteroid_belt[30]; /* 총 구역 */
     const char* cluster_start_addresses[2]; /* 소행성 지대들의 시작 주소 */
     size_t cluster_lengths[2]; /* 몇개의 소행성 지대가 있는지 */
-    size_t out_longest_safe_area_length = 0; /* 가장 긴 안정구역 길이 */
+    size_t out_longest_safe_area_length = 0; /* 가장 긴 안전구역 길이 */
     size_t* out_longest_safe_area_length_p = &out_longest_safe_area_length; /* 가장 긴 안정구역 길이 메모리의 주소 */
     const char* longest_safe_cluster_start_address;
 
@@ -23,5 +19,12 @@ int main(void)
     cluster_lengths[0] = 15U;
     cluster_lengths[1] = 25U;
 
+    printf("cyber_asteroid_belt address: %p\n", (void*)cyber_asteroid_belt);
     longest_safe_cluster_start_address = get_longest_safe_zone_or_null(cyber_asteroid_belt, 30, cluster_start_addresses, cluster_lengths, 2, out_longest_safe_area_length_p); /* longest_safe_cluster_start_address: (cyber_asteroid_belt + 12), out_longest_safe_area_length: 15 */
+    printf("expected address: %p\n", (void*)&cyber_asteroid_belt[12]);
+    printf("result address: %p\n", (void*)longest_safe_cluster_start_address);
+    printf("expected Longest length value: %d\n", 15);
+    printf("result Longest length value: %d\n", out_longest_safe_area_length);
+
+    return 0;
 }
