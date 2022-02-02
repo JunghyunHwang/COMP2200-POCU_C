50605218b028ac85068ad19943d0ab0ff29cdb56
Ja_Hwang <dmagk560@gmail.com>
2022-02-02 20:18:28 +0900

    Fix fwrite parameter value

--- Lab5/receipter.c
+++ Lab5/receipter.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#include <time.h>
+#include <string.h>
 #include "receipter.h"
 
 #define RESTAURANT_NAME ("Charles' Seafood")
-#define DELIMTER_LINE ("--------------------------------------------------")
+#define RESTAURANT_NAME_LENGTH (16)
+#define DELIMTER_LINE1 ("--------------------------------------------------")
+#define DELIMTER_LINE2 ("==================================================")
 #define MAX_ORDER_COUNT (10)
-#define MAX_LINE_COUNT (51)
+#define MAX_LINE_COUNT (50)
 #define MAX_MESSAGE_COUNT (76)
 #define TAX_RATE (0.05)
 #define TAX_NUMBER ("Tax#-51234")
@@ -58,7 +60,7 @@ void set_message(const char* message)
 int print_receipt(const char* filename, time_t timestamp)
 {
     FILE* stream;
-    char out_str[MAX_LINE_COUNT];
+    char out_str[MAX_LINE_COUNT + 1];
     double total_amount;
     double tax;
     char white_space = ' ';
@@ -83,45 +85,45 @@ int print_receipt(const char* filename, time_t timestamp)
     timer = *gmtime(&timestamp);
 
     stream = fopen(filename, "w");
-
-    fwrite(RESTAURANT_NAME, sizeof(RESTAURANT_NAME), 1, stream);
+    
+    fwrite(RESTAURANT_NAME, sizeof(char), RESTAURANT_NAME_LENGTH, stream);
     fwrite("\n", sizeof(char), 1, stream);
 
-    fwrite(DELIMTER_LINE, sizeof(DELIMTER_LINE), 1, stream);
+    fwrite(DELIMTER_LINE1, sizeof(char), MAX_LINE_COUNT, stream);
     fwrite("\n", sizeof(char), 1, stream);
 
     sprintf(out_str, "%04d-%02d-%02d%c%02d:%02d:%02d%26c%05d", timer.tm_year + 1900, timer.tm_mon + 1, timer.tm_mday, white_space, timer.tm_hour, timer.tm_min, timer.tm_sec, white_space, s_today_order_count);
-    fwrite(out_str, sizeof(out_str), 1, stream);
+    fwrite(out_str, sizeof(char), MAX_LINE_COUNT, stream);
     fwrite("\n", sizeof(char), 1, stream);
 
-    fwrite(DELIMTER_LINE, sizeof(DELIMTER_LINE), 1, stream);
+    fwrite(DELIMTER_LINE1, sizeof(char), MAX_LINE_COUNT, stream);
     fwrite("\n", sizeof(char), 1, stream);
 
     for (i = 0; i < s_number_of_orders; ++i) {
         sprintf(out_str, "%33s%17.2f", s_order_list[i], s_item_price[i]);
-        fwrite(out_str, sizeof(out_str), 1, stream);
+        fwrite(out_str, sizeof(char), MAX_LINE_COUNT, stream);
         fwrite("\n", sizeof(char), 1, stream);
     }
 
     fwrite("\n", sizeof(char), 1, stream);
 
     sprintf(out_str, "%33s%17.2f", "Subtotal", s_subtotal);
-    fwrite(out_str, sizeof(out_str), 1, stream);
+    fwrite(out_str, sizeof(char), MAX_LINE_COUNT, stream);
     fwrite("\n", sizeof(char), 1, stream);
 
 
     if (s_tip != 0) {
         sprintf(out_str, "%33s%17.2f", "Tip", s_tip);
-        fwrite(out_str, sizeof(out_str), 1, stream);
+        fwrite(out_str, sizeof(char), MAX_LINE_COUNT, stream);
         fwrite("\n", sizeof(char), 1, stream);
     }
 
     sprintf(out_str, "%33s%17.2f", "Tax", tax);
-    fwrite(out_str, sizeof(out_str), 1, stream);
+    fwrite(out_str, sizeof(char), MAX_LINE_COUNT, stream);
     fwrite("\n", sizeof(char), 1, stream);
 
     sprintf(out_str, "%33s%17.2f", "Total", total_amount);
-    fwrite(out_str, sizeof(out_str), 1, stream);
+    fwrite(out_str, sizeof(char), MAX_LINE_COUNT, stream);
     fwrite("\n\n", sizeof(char), 2, stream);
 
     if (s_message[0] != '\0') {
@@ -134,21 +136,19 @@ int print_receipt(const char* filename, time_t timestamp)
             fwrite(p_str_start, sizeof(char), MAX_LINE_COUNT - 2, stream);
             fwrite("\n", sizeof(char), 1, stream);
 
-            p_str_start += 50;
-            s_msg_len -= 50;
+            p_str_start += MAX_LINE_COUNT;
+            s_msg_len -= MAX_LINE_COUNT;
 
             fwrite(p_str_start, sizeof(char), s_msg_len, stream);
             fwrite("\n", sizeof(char), 1, stream);
         }
     }
 
-    sprintf(out_str, "%s", "==================================================");
-    fwrite(out_str, sizeof(out_str), 1, stream);
+    fwrite(DELIMTER_LINE2, sizeof(char), MAX_LINE_COUNT, stream);
     fwrite("\n", sizeof(char), 1, stream);
 
     sprintf(out_str, "%50s", TAX_NUMBER);
-    fwrite(out_str, sizeof(out_str), 1, stream);
-    fwrite("\n", sizeof(char), 1, stream);
+    fwrite(out_str, sizeof(char), MAX_LINE_COUNT, stream);
 
     fclose(stream);
 
