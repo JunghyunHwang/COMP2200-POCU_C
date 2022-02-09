9962bf1c9260c362e0b8dde6dc67c22ee11a0d22
Ja_Hwang <dmagk560@gmail.com>
2022-02-09 13:57:08 +0900

    Add get character info

--- Lab6/character_deserializer.c
+++ Lab6/character_deserializer.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include "character_deserializer.h"
 
+#define MAX_LINE_COUNT (256)
+#define MAX_CHARACTER_NAME (51)
+
 int get_character(const char* filename, character_v3_t* out_character)
 {
     int result_version = 0;
@@ -10,23 +13,33 @@ int get_character(const char* filename, character_v3_t* out_character)
 
     /* chekc version*/
     stream = fopen(filename, "r");
+    printf("File name: %s\n", filename);
 
-    while ((ch = fgetc(stream)) != EOF || result_version == 0) {
+    while ((ch = fgetc(stream)) != EOF && result_version == 0) {
         switch (ch) {
         case ':':
             result_version = VERSION_1;
             rewind(stream);
+            printf("version: %d\n", result_version);
+            
             get_character_by_version1(stream, out_character);
+            printf("=================\n");
             break;
         case ',':
             result_version = VERSION_2;
             rewind(stream);
+            printf("version: %d\n", result_version);
+
             get_character_by_version2(stream, out_character);
+            printf("=================\n");
             break;
         case '|':
             result_version = VERSION_3;
             rewind(stream);
+            printf("version: %d\n", result_version);
+
             get_character_by_version3(stream, out_character);
+            printf("=================\n");
             break;
         default:
             break;
@@ -40,17 +53,26 @@ int get_character(const char* filename, character_v3_t* out_character)
     /* get_character_info_by_version*/
 }
 
-void get_character_by_version1(FILE* stream, character_v3_t out_character)
+void get_character_by_version1(FILE* stream, character_v3_t* out_character)
 {
-    
+    char info[MAX_LINE_COUNT];
+
+    fgets(info, MAX_LINE_COUNT, stream);
+    printf("%s\n", info);
 }
 
-void get_character_by_version2(FILE* stream, character_v3_t out_character)
+void get_character_by_version2(FILE* stream, character_v3_t* out_character)
 {
+    char info[MAX_LINE_COUNT];
     
+    fgets(info, MAX_LINE_COUNT, stream);
+    printf("%s\n", info);
 }
 
-void get_character_by_version3(FILE* stream, character_v3_t out_character)
+void get_character_by_version3(FILE* stream, character_v3_t* out_character)
 {
+    char info[MAX_LINE_COUNT];
     
+    fgets(info, MAX_LINE_COUNT, stream);
+    printf("%s\n", info);
 }
--- Lab6/main.c
+++ Lab6/main.c
@@ -4,10 +4,12 @@
 
 int main(void)
 {
-    character_v3_t superman_v1;
-    int version = get_character("Flash_v3.txt", &superman_v1);
+    character_v3_t character;
 
-    printf("File version: %d\n", version);
+    get_character("Superman_v1.txt", &character);
+    get_character("Batman_v2.txt", &character);
+    get_character("Wonderwoman_v3.txt", &character);
+    get_character("Flash_v3.txt", &character);
 
     return 0;
 }
