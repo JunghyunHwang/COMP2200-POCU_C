7ed79bc708496e442163f361cad3d51b6f86e496
Ja_Hwang <dmagk560@gmail.com>
2022-03-13 02:19:31 +0900

    Add print matched parentheses

--- Lab8/parentheses.c
+++ Lab8/parentheses.c
@@ -4,14 +4,14 @@
 #include <assert.h>
 #include "parentheses.h"
 
-int is_parenthesis(const char* opening_parentheses, const char character)
+int is_parenthesis(const char* parentheses, const char character)
 {
     int result_index = -1;
-    const char* p_current = opening_parentheses;
+    const char* p_current = parentheses;
 
     while (*p_current != '\0') {
         if (*p_current == character) {
-            result_index = p_current - opening_parentheses;
+            result_index = p_current - parentheses;
             break;
         }
 
@@ -21,37 +21,42 @@ int is_parenthesis(const char* opening_parentheses, const char character)
     return result_index;
 }
 
-size_t get_matching_parentheses(parenthesis_t* out_parentheses, size_t max_size, const char* str)
+void print_matched_parentheses(parenthesis_t* out_prentheses, size_t count)
 {
-    /* 
-        1. str을 처음 부터 끝까지 훑으면서 여는 괄호는 queue으로, 닫는 괄호는 stack으로 저장
-        2. queue와 stack을 연결 시켜서 
-    */
-
-    size_t parentheses_matched_count = 0;
-    const char* p_current;
-    const char* opening_parentheses = "{([<"; /* 쌍이되는 괄호끼리 같은 index여야 함 */
-    const char* closing_parentheses = "})]>"; /* 쌍이되는 괄호끼리 같은 index여야 함 */
-    int matched_closing_index;
     size_t i;
 
-    p_current = str;
+    for (i = 0; i < count; ++i) {
+        printf("======= No. %d =======\n", i);
+        printf("opening index: %d\n", out_parentheses[i].opening_index);
+        printf("closing index: %d\n", out_parentheses[i].closing_index);
+        puts("=====================");
+    }
+}
+
+size_t get_matching_parentheses(parenthesis_t* out_parentheses, size_t max_size, const char* str)
+{
+    size_t parentheses_matched_count = 0;
+    const char* p_current = str;
+    const char* opening_parentheses = "{([<"; /* 쌍이되는 괄호끼리 같은 index여야 함 */
+    const char* closing_parentheses = "})]>"; /* 쌍이되는 괄호끼리 같은 index여야 함 */
+    int matching_closing_index;
+
     while (*p_current != '\0') {
-        if ((matched_closing_index = is_parenthesis(opening_parentheses, *p_current)) >= 0) {
+        if ((matching_closing_index = is_parenthesis(opening_parentheses, *p_current)) >= 0) {
             const char* p_seek = p_current + 1;
             size_t same_parenthesis_count = 0;
 
             while (*p_seek != '\0') {
-                if (is_parenthesis(closing_parentheses, *p_seek) == matched_closing_index) {
+                if (*p_current == *p_seek) {
+                    ++same_parenthesis_count;
+                } else if (is_parenthesis(closing_parentheses, *p_seek) == matching_closing_index) {
                     if (same_parenthesis_count == 0) {
                         out_parentheses[parentheses_matched_count].opening_index = p_current - str;
                         out_parentheses[parentheses_matched_count++].closing_index = p_seek - str;
                         break;
-                    } else {
-                        --same_parenthesis_count;
                     }
-                } else if (*p_current == *p_seek) {
-                    ++same_parenthesis_count;
+
+                    --same_parenthesis_count;
                 }
 
                 ++p_seek;
@@ -65,12 +70,7 @@ size_t get_matching_parentheses(parenthesis_t* out_parentheses, size_t max_size,
         ++p_current;
     }
 
-    for (i = 0; i < parentheses_matched_count; ++i) {
-        printf("======= No. %d =======\n", i);
-        printf("opening index: %d\n", out_parentheses[i].opening_index);
-        printf("closing index: %d\n", out_parentheses[i].closing_index);
-        puts("=====================");
-    }
+    print_matched_parentheses(out_parentehses, parentheses_matched_count);
 
     return parentheses_matched_count;
 }
