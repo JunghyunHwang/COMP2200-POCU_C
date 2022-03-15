03f0b5999b745ff11e4587954ce49bb90b3575f6
junghyunhwang <dmagk560@gmail.com>
2022-03-15 16:51:52 +0900

    Add bracket for priority

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -1,16 +1,16 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-#define MAX(x, y) (x >= y ? x : y)
+#define MAX(x, y) ((x >= y ? x : y))
 
-#define MIN(x, y) (x <= y ? x : y)
+#define MIN(x, y) ((x <= y ? x : y))
 
-#define ABS(x) (x >= 0 ? x : x * -1)
+#define ABS(x) ((x >= 0 ? x : x * -1))
 
 #define RANGE(curr, min, max) for (curr = min; curr <= max; ++curr)
 
 #define RANGE_DESC(curr, max, min) for (curr = max; curr >= min; --curr)
-/*
+
 #define SET(ary, start, count, value)   \
 {                                       \
 	size_t i = start;                   \
@@ -19,8 +19,5 @@
         ary[i] = value;                 \
 	}                                   \
 }                                       \
-*/
-
-#define SET(ary, start, count, value) memset(ary + start, value, count);
 
 #endif /* MACROS_H */
