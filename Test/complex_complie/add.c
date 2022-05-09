c5783a1b3654b79e9349eff197cc1aa3ce9783cf
Ja_Hwang <dmagk560@gmail.com>
2022-05-10 00:40:36 +0900

    Add complex complie

---
+++ Test/complex_complie/add.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "calculator.h"
+
+int add(int a, int b)
+{
+    printf("Result: %d\n", a + b);
+    return a + b;
+}
---
+++ Test/complex_complie/sub.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#ifdef __cplusplus 
+extern "C" 
+{ 
+#endif 
+    int printf... 
+    int sprintf... 
+     ... 
+#ifdef __cplusplus 
+} 
+#endif 
+
+int sub(int a, int b)
+{
+    std::cout << x << std::endl;
+
+    return a + b;
+}
