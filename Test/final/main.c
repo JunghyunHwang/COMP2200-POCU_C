a95556e7d2c685e04e59cc9acbb1c2fc86f5f5ca
Ja_Hwang <dmagk560@gmail.com>
2022-08-05 23:14:19 +0900

    Fix typo

--- Test/final/main.c
+++ Test/final/main.c
@@ -1,10 +1,12 @@
+#include <stdio.h>
+
 #define MULTIPLY(a, b) a * b
 #define ADD(a, b) a + b;
 
 void testMacro();
 void testCharacter();
 
-int main(void) 
+int main(void)
 {
     testMacro();
     testCharacter();
@@ -19,13 +21,13 @@ void testMacro()
 
 void testCharacter()
 {
-    char word0 = "I love C";
+    char word0[] = "I love C";
     char* p0 = word0;
-    char** word_p0 = &p;
+    char** word_p0 = &p0;
 
-    char word1 = "C++";
+    char word1[] = "C++";
     char* p1 = word1;
-    char** word_p1 = p1;
+    char** word_p1 = &p1;
 
     **word_p0 = **word_p1;
     word_p0 = NULL;
